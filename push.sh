f2aad79d3ecdb1048221393f10c9b5b25401fed6
Tensangna Julius <68362477+Tagong40@users.noreply.github.com>
2023-12-06 02:11:00 +0000

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,31 +1,46 @@
-#!binbash
+#!/bin/bash
 
-# Set your GitHub username and repository name
-USERNAME=your_username
-REPO=your_repository
+# Check if running locally or on GitLab CI/CD
+if [ "$CI" == "true" ] || [ "$LOCAL" == "true" ]; then
+    # Set local API URL
+    API_URL="http://127.0.0.1:8000/api/v1/"
+else
+    # Set production API URL
+    API_URL="https://er.glofacts.com/api/v1/"
+fi
 
-# Set the branch you want to push
-BRANCH=main
+# Replace the placeholder in the JavaScript file with the determined API_URL
+sed -i "s|const API_URL = \"http://127.0.0.1:8000/api/v1/\";|const API_URL = \"${API_URL}\";|g" src/store/axios.tsx
+
+
+
 
 # Check if a commit message is provided as an argument
-if [ -z $1 ]; then
-  echo Usage $0 commit_message
+if [ -z "$1" ]; then
+  echo "Usage: $0 <commit_message>"
   exit 1
 fi
 
 # Use the provided commit message
-COMMIT_MESSAGE=$1
+COMMIT_MESSAGE="$1"
 
 # Add and commit changes
 git add .
-git commit -m $COMMIT_MESSAGE
+git commit -m "$COMMIT_MESSAGE"
 
 # Push changes to GitHub
 git push origin $BRANCH
 
 # Check if the push was successful
-if [ $ -eq 0 ]; then
-  echo Code pushed successfully to GitHub
+if [ $? -eq 0 ]; then
+  echo "Code pushed successfully to GitHub"
 else
-  echo Error Failed to push code to GitHub
+  echo "Error: Failed to push code to GitHub"
 fi
+
+API_URL_LOCAL="http://127.0.0.1:8000/api/v1/"
+
+
+sed -i "s|const API_URL = \"https://er.glofacts.com/api/v1/\";|const API_URL = \"${API_URL_LOCAL}\";|g" src/store/axios.tsx
+
+yarn dev
